9b76d1b91d6f253b85e57f3d69b33b04e69c98ba
user.email <martin.macko@vacuumlabs.com>
2017-03-21 14:20:15 +0100

    Adjust pylint rules

--- skywall/core/database.py
+++ skywall/core/database.py
@@ -1,3 +1,4 @@
+# pylint: disable=invalid-name, global-statement
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
