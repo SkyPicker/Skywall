039856da0c31910d52f724f83bae7d9ec4d32f5b
Martin Macko <martin.macko@vacuumlabs.com>
2017-06-09 20:07:38 +0200

    Add CheckBoxField

--- frontend/utils/fields.js
+++ frontend/utils/fields.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {If} from 'jsx-control-statements'
-import {FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap'
+import {FormGroup, ControlLabel, FormControl, HelpBlock, Checkbox} from 'react-bootstrap'
 import {Field} from './forms'
 
 
@@ -24,6 +24,36 @@ export class TextField extends Field {
             onChange={this.handleChange}
             disabled={!this.isEditing()}
         />
+        <FormControl.Feedback />
+        <HelpBlock>{this.validationHelp()}</HelpBlock>
+      </FormGroup>
+    )
+  }
+}
+
+export class CheckBoxField extends Field {
+  constructor(opts = {}) {
+    super({
+      format: (value) => Boolean(value),
+      ...opts,
+    })
+  }
+  handleChange(e) {
+    this.form.change(this.key, e.target.checked)
+  }
+  render() {
+    return (
+      <FormGroup
+          controlId={this.key}
+          validationState={this.validationState()}
+      >
+        <Checkbox
+            checked={this.value() || false}
+            onChange={this.handleChange}
+            disabled={!this.isEditing()}
+        >
+          {this.opts.label}
+        </Checkbox>
         <FormControl.Feedback />
         <HelpBlock>{this.validationHelp()}</HelpBlock>
       </FormGroup>
