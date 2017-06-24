b1659ec71cce2a87cb1c21516d33054fa50df2e9
Martin Macko <martin.macko@vacuumlabs.com>
2017-06-24 19:00:19 +0200

    Add get_group_clients utility function

--- skywall/models/groups.py
+++ skywall/models/groups.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from skywall.core.database import Model
 from skywall.core.signals import Signal
+from skywall.models.clients import Client
 
 
 before_group_create = Signal('before_group_create')
@@ -23,3 +24,15 @@ class Group(Model):
 
     def __repr__(self):
         return '<Group id={0.id} name={0.name}>'.format(self)
+
+
+def get_group_clients(session, group):
+    """
+    We can't use directly `group.clients` for the DEFAULT group because it is represented by `None`. Unless we are
+    sure the group is not DEFAULT, we must use `get_group_clients(session, group)` instead.
+    """
+    if group is None:
+        # pylint: disable=singleton-comparison
+        return session.query(Client).filter(Client.group_id == None).all()
+    else:
+        return group.clients
